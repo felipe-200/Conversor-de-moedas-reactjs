7a3eccd52df8b71bb7c6c12f1bbf7df0c31b6782
Felipe Jorge <laranjiaplay20@gmail.com>
2021-03-05 22:28:38 -0300

    Versão 1 do conversor de moedas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
+import { useEffect, useState } from "react";
 
 function App() {
+  //const [valorMoedaA, setValorMoedaA] = useState(0)
+  const [valorMoedaB, setValorMoedaB] = useState(1)
+  const [codeMoedaB, setCodeMoedaB] = useState()
+  const [valueInput, setValueInput] = useState(null)
+  const [resultCambio, setResultCambio] = useState(null)
+
+  //
+  function valueCoinAPI(coin) {
+    fetch(`https://economia.awesomeapi.com.br/all/${coin}`)
+      .then(response => response.json())
+      .then((data) => {
+        const { code, high } = data.USD
+        setValorMoedaB(valorMoedaB * high)
+        setCodeMoedaB(code)
+      })
+  }
+  //
+  useEffect(() => {
+    valueCoinAPI(`USD-BRL`)
+  }, [])
+  //
+
+  //
+  function handleValueInput(data) {
+    setValueInput(data.target.value)
+  }
+  //
+  function resultCambioCoin() {
+    setResultCambio(valorMoedaB * valueInput)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section>
+
+        <h3>Conversor de Moedas</h3>
+        <span> Resultado: {resultCambio} </span>
+        <input type="text" onChange={handleValueInput} placeholder="Digite algum valor" />
+        <button onClick={resultCambioCoin}>Converter</button>
+        <button onClick={() => { setResultCambio(null) }}>Limpar</button>
+      </section>
     </div>
   );
 }
